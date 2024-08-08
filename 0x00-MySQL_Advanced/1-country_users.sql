82e4683ab15f9110283b6b80669fea172187bc6a
Lumasia <lumasiastancelaus@gmail.com>
2024-08-08 14:29:55 +0300

    Corrected table name to required users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Create a table users with enum values
-CREATE TABLE IF NOT EXISTS users_1 (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
