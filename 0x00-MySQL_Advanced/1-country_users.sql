17d61f48060270ab3611b5c9ac940b02726edbf9
Lumasia <lumasiastancelaus@gmail.com>
2024-08-08 14:27:46 +0300

    Users table with ENUM values

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create a table users with enum values
+CREATE TABLE IF NOT EXISTS users_1 (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+) ENGINE=InnoDB;
