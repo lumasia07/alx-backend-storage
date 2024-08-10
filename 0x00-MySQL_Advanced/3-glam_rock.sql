d63192292175e015e60784bf29e125f53d125f83
Lumasia <lumasiastancelaus@gmail.com>
2024-08-10 13:06:16 +0300

    glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,15 @@
+-- Lists all bands with Glam rock as main style
+-- Rank by their longevity
+
+SELECT
+	band_name,
+  CASE
+  	WHEN split IS NULL THEN 2022 - formed
+    ELSE split - formed
+  END AS lifespan
+FROM
+	metal_bands
+WHERE
+	style = 'Glam rock'
+ORDER BY
+	lifespan DESC, band_name ASC;
