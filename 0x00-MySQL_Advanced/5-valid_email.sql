e0c569cf8603d11ffa7914d39e4be017aebcb049
Lumasia <lumasiastancelaus@gmail.com>
2024-08-10 14:48:39 +0300

    email validator delimitor

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,13 @@
 -- Create the trigger to reset valid_email when email is changed
+DELIMITER //
+
 CREATE TRIGGER reset_valid_email_before_update
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
-    END IF;
-END;
+    END IF//
+END//
+
+DELIMITER ;
