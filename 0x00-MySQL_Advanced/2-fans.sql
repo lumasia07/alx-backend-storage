e4f07d73384d75cd8a12972655a4f38f491c9c8d
Lumasia <lumasiastancelaus@gmail.com>
2024-08-10 12:25:47 +0300

    Group fans by origin

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,11 @@
+-- Ranks country origin of bands
+-- Order by number of non-unique fans
+SELECT
+	origin,
+    SUM(fans) as nb_fans
+FROM
+	metal_bands
+GROUP BY
+ 	origin
+ORDER BY
+	nb_fans DESC;
