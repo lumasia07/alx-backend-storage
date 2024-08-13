376ea5aa40d6bbe3befa6adb8e4e052b7d576fdb
Lumasia <lumasiastancelaus@gmail.com>
2024-08-13 23:56:16 +0300

    insert with python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,17 @@
+#!/usr/bin//env python3
+"""Insert with args"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new doc in collection
+
+    Params:
+    mongo_collection: collection object
+    **kwargs: key-value pair for field-values
+
+    Returns:
+    ObjectId: _id of newly inserted object
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
