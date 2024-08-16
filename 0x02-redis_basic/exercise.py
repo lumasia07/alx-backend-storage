064994ec90f1d486579aaa4b168408712e7313f3
Lumasia <lumasiastancelaus@gmail.com>
2024-08-16 11:54:59 +0300

    Added getter methods for Redis Cache Class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writes a string to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,7 +18,25 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key"""
         key = str(uuid.uuid4())
-
         self._redis.set(key, data)
-
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes],
+                                                  Union[str, int, bytes, float]
+                                                  ]] = None) -> Optional[
+                                                      Union[
+                                                          str, int,
+                                                          bytes, float]]:
+        """Getter method for key"""
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Getter method for string"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Getter method for bytes"""
+        return self.get(key, lambda d: int(d))
